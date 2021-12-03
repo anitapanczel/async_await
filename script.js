4ded0b9477e8f8151b92f3c6ae1ca878a8b5b023
anitapanczel <anita@panczel.hu>
2021-12-03 12:17:39 +0100

    renderCountries

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-window.addEventListener("load", init);
+const renderCountries = (arr) => {
 
-function init() {
-    console.log("hello");
+
+    let returnHTML = "";
+
+    for (const country of arr) {
+        console.log(country.name.official);
+        returnHTML += `
+        <div>${country.name.official}
+        </div>
+        `
+    }
+    return returnHTML;
 }
+
+const renderFlags = (arr) => {
+    let returnHTML = "";
+    for (const flag of arr) {
+        returnHTML += `
+        <img src="${flag.flags.png}">`
+
+    }
+    return returnHTML;
+}
+
+async function init() {
+
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+
+    //renderCountries(countryArr);
+
+    document.getElementById("root").insertAdjacentHTML("beforeend", `<div id="countryNames">${renderCountries(countryArr)}</div>`);
+    document.getElementById("root").insertAdjacentHTML("beforeend", `<div id="countryFlags">${renderFlags(countryArr)})</div>`);
+}
+
+window.addEventListener("load", init());
